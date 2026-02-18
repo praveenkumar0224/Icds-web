d12b7f46aed25770fd1d73e6607acae0f64192c9
praveenkumar0224 <praveenkumar@madhifoundation.org>
2026-02-18 14:54:26 +0530

    environment  changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://icds.xenovex.com/api/v3/',
+  apiUrl: 'http://13.204.36.2:5412/v3/',
   production: false,
   appName: 'ICDS Web Dashboard'
 };
